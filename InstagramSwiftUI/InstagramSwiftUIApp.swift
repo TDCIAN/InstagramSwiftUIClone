740cc79aaeb6000475e09a658abc2effd4d2b144
Jeongmin <tdcian71@gmail.com>
2021-05-15 21:19:50 +0900

    Initial Commit

---
+++ InstagramSwiftUI/InstagramSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramSwiftUIApp.swift
+//  InstagramSwiftUI
+//
+//  Created by APPLE on 2021/05/15.
+//
+
+import SwiftUI
+
+@main
+struct InstagramSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
