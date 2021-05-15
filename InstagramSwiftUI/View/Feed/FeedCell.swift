a6cf04a67894fc72233122d800478c76ff1f7353
Jeongmin <tdcian71@gmail.com>
2021-05-15 22:28:36 +0900

    3강 진행 중

---
+++ InstagramSwiftUI/View/Feed/FeedCell.swift
@@ -0,0 +1,22 @@
+//
+//  FeedCell.swift
+//  InstagramSwiftUI
+//
+//  Created by APPLE on 2021/05/15.
+//
+
+import SwiftUI
+
+struct FeedCell: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            
+        }
+    }
+}
+
+struct FeedCell_Previews: PreviewProvider {
+    static var previews: some View {
+        FeedCell()
+    }
+}
